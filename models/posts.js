677d46500bf427dc036bdd201d92775c4dcc0f3c
boullif <adoula136@gmail.com>
2021-04-30 02:45:34 +0200

    ....

--- models/posts.js
+++ models/posts.js
@@ -60,15 +60,7 @@ const postSchema = new mongoose.Schema(
         "la date de fin doit étre supérieure a la date du début",
       ],
     },
-    geometry: [
-      {
-        type: {
-          type: String,
-          default: "Point",
-        },
-        coordinates: [Number],
-      },
-    ],
+    geometry: [Number],
     owner: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
