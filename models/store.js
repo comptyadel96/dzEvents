7cfc68d1548ea914d70821fc0f56111b70996e6b
boullif <adoula136@gmail.com>
2021-05-26 14:31:43 +0200

    remove min length from description in store schema

--- models/store.js
+++ models/store.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 
 const storeSchema = new mongoose.Schema(
   {
@@ -6,36 +6,35 @@ const storeSchema = new mongoose.Schema(
       type: String,
       minlength: 5,
       maxlength: 1024,
-      required: [true, 'veuillez donner un titre à votre article ou service!'],
+      required: [true, "veuillez donner un titre à votre article ou service!"],
       trim: true,
     },
     prix: {
       type: String,
       minlength: 1,
       maxlength: 100,
-      required: [true, 'veuillez défénir un prix à votre article ou service !'],
+      required: [true, "veuillez défénir un prix à votre article ou service !"],
     },
     wilaya: {
       type: String,
       minlength: 2,
       maxlength: 100,
-      required: [true, 'vous devez spécifiez votre  wilaya'],
+      required: [true, "vous devez spécifiez votre  wilaya"],
     },
     description: {
       type: String,
-      minlength: 10,
       maxlength: 2200,
     },
-    photos: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Photo' }],
+    photos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Photo" }],
     owner: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
+      ref: "User",
       // required: true,
     },
   },
   { timestamps: true }
 )
-storeSchema.index({ article: 'text' })
+storeSchema.index({ article: "text" })
 
-const Store = mongoose.model('Store', storeSchema)
+const Store = mongoose.model("Store", storeSchema)
 module.exports = { Store }
