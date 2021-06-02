fbc1cd06987d6f092903b51c2c4a1e9458d3500b
boullif <adoula136@gmail.com>
2021-06-03 01:17:08 +0200

    set thr auth middlware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,8 +1,13 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken")
 
 module.exports = async function (req, res, next) {
-  const token = req.header('x-auth-token')
-  if (!token) return res.status(403).send('vous devez etre connecter pour intéragir avec ce contenu')
+  const token = req.header("x-auth-token")
+  if (!token)
+    return res
+      .status(401)
+      .send({
+        erreure: "vous devez etre connecter pour intéragir avec ce contenu",
+      })
   try {
     const encoded = await jwt.verify(token, process.env.JWT_TOKEN_KEY)
     req.user = encoded
